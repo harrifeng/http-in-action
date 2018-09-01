c693755b169aa40d5285d788a8a7306fd3856639
Haoran Feng <harrifeng@gmail.com>
2018-09-01 08:13:19 +0000

    new ok

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hello")
+const counter = (state = 0, action) => {
+  switch (action.type) {
+  case 'INCREMENT':
+    return state +1;
+  case 'DECREMENT':
+    return state -1;
+  default:
+    return state;
+  }
+}
+
+const { createStore } = Redux;
+
+const store = createStore(counter);
+
+console.log(store.getState())
+
+store.dispatch({type: 'INCREMENT'});
+console.log(store.getState())
